c40c8bde19b02d4731cb5cc53583a0c578bdfe0a
IqraaSW <49357997+IqraaSW@users.noreply.github.com>
2019-04-14 19:51:28 -0700

    Add files via upload

---
+++ Lab 2 Task/RelationalOperators.java
@@ -0,0 +1,13 @@
+public class RelationalOperators{
+public static void main(String args[]){
+int num1=5;
+int num2=6;
+if(num1>num2){
+System.out.println("Number1 is greater than number2");
+}
+else 
+{
+System.out.println("Number2 is greater than number1");
+}
+}
+}
---
+++ Lab 2 Task/Task4.java
@@ -0,0 +1,14 @@
+public class Task4{
+public static void main(String args[])
+{
+int r=2;
+float f=3.14f;
+double d=2;
+long diameter=2*r;
+double circumference=2*f*r;
+double Area=f*r*r;
+System.out.println("Diameter:"+ diameter);
+System.out.println("Circumference:"+ circumference);
+System.out.println("Area of circle:"+ Area);
+}
+}
